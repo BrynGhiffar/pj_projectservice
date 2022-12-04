c7c22a4698f9b1d75fb089a84dbf292af85067a7
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2022-12-04 23:57:01 +0700

    added certificate configuration

--- adapter/repository/config/config.py
+++ adapter/repository/config/config.py
@@ -1,10 +1,12 @@
 import os
 from pymongo import MongoClient
+import certifi
 
 def get_database():
+    ca = certifi.where()
     CONNECTION_STRING = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("MONGO_DB_NAME")
 
-    client = MongoClient(CONNECTION_STRING)
+    client = MongoClient(CONNECTION_STRING, tlsCAFile=ca)
 
     return client[DB_NAME]
