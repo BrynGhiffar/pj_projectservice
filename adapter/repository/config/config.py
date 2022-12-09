704b9ac59ae6f15107c163e1f7dd91a5fec2ea2c
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2022-12-09 09:48:05 +0700

    added database configuration setting to readme.md

--- adapter/repository/config/config.py
+++ adapter/repository/config/config.py
@@ -7,6 +7,9 @@ def get_database():
     CONNECTION_STRING = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("MONGO_DB_NAME")
 
-    client = MongoClient(CONNECTION_STRING, tlsCAFile=ca)
-
+    client = MongoClient(CONNECTION_STRING)
+    # use this for accessing a mongodb+srv database instead
+    # client = MongoClient(CONNECTION_STRING, tlsCAFile=ca)
+    if not DB_NAME:
+        DB_NAME = "mongodb://localhost:27017/"
     return client[DB_NAME]
